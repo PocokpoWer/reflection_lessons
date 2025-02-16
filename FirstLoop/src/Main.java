a891782bfd66f61c9964de6e4b2e508b17030adb
Tibi <farkas.tibor1105@gmail.com>
2025-02-16 23:28:19 +0100

    Program done

---
+++ FirstLoop/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a text: ");
+        String stringInput = scanner.nextLine();
+        System.out.println("Write a number: ");
+        int loopNumber = scanner.nextInt();
+        if (loopNumber <= 0) {
+            System.out.println("Error");
+        } else {
+            for (int i = 0; i < loopNumber; i++) {
+                System.out.println(stringInput);
+            }
+        }
+    }
+}
