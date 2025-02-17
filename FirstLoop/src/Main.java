d0255df37027aaf3a8a821efb0f35f53f8fdeccb
Tibi <farkas.tibor1105@gmail.com>
2025-02-17 09:39:59 +0100

    Repaired

--- FirstLoop/src/Main.java
+++ FirstLoop/src/Main.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
+        //Data input
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write a text: ");
         String stringInput = scanner.nextLine();
         System.out.println("Write a number: ");
         int loopNumber = scanner.nextInt();
-
+        //Print text
         if (loopNumber <= 0) {
             System.out.println("Error");
         } else {
