66d296e919f2a972689b493aa3a12b63da28c185
Tibi <farkas.tibor1105@gmail.com>
2025-04-14 09:34:08 +0200

    add 4d array tasks practice

---
+++ array-fun-4d/src/Main.java
@@ -0,0 +1,302 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 16:");
+        String[][][][] arrayOfTask16 = {
+                {
+                        {
+                                {"hello", "world", "Java"},
+                                {"apple", "banana", "TREE"}
+                        },
+                        {
+                                {"test", "data", "PYTHON"},
+                                {"code", "snippets", "123"}
+                        }
+                },
+                {
+                        {
+                                {"lower", "CASE", "MixEd"},
+                                {"yes", "no", "maybe"}
+                        },
+                        {
+                                {"only", "lowercase", "strings"},
+                                {"OKAY", "fine", "great"}
+                        }
+                }
+        };
+        System.out.println(countLowerCaseWords(arrayOfTask16));
+
+        System.out.println("Task 17:");
+        String[][][][] arrayOfTask17 = {
+                {
+                        {
+                                {"apple", "banana"},
+                                {"cherry", "date"}
+                        },
+                        {
+                                {"elephant", "fish"},
+                                {"grape", "hippo"}
+                        }
+                },
+                {
+                        {
+                                {"iguana", "jaguar"},
+                                {"koala", "lemur"}
+                        },
+                        {
+                                {"monkey", "narwhal"},
+                                {"octopus", "penguin"}
+                        }
+                }
+        };
+        System.out.println(Arrays.deepToString(reverseString(arrayOfTask17)));
+
+        System.out.println("Task 18:");
+        String[][][][] arrayOfTask18 = {
+                {
+                        {
+                                {"hello", "world"},
+                                {"java", "rocks"}
+                        },
+                        {
+                                {"foo", "bar", "baz"}
+                        }
+                },
+                {
+                        {
+                                {"openai"},
+                                {"gpt", "chat"}
+                        }
+                }
+        };
+        System.out.println(calculateAllStringAverage(arrayOfTask18));
+
+        System.out.println("Task 19:");
+        String[][][][] arrayOfTask19 = {
+                {
+                        {
+                                {"cat", "education"},
+                                {"sky", "audio"}
+                        },
+                        {
+                                {"tree", "why"},
+                                {"unique", "xylophone"}
+                        }
+                },
+                {
+                        {
+                                {"run", "queue"},
+                                {"box", "algorithm"}
+                        },
+                        {
+                                {"python", "java"},
+                                {"umbrella", "oop"}
+                        }
+                }
+        };
+        System.out.println(Arrays.deepToString(getStringsWithTwoVowel(arrayOfTask19)));
+
+        System.out.println("Task 20:");
+        String[][][][] arrayOfTask20 = {
+                {
+                        {
+                                {"apple", "banana", "kiwi"},
+                                {"orange", "pear", "a"}
+                        },
+                        {
+                                {"grape", "plum", "x"},
+                                {"melon", "fig", "b"}
+                        }
+                },
+                {
+                        {
+                                {"mango", "lemon", "abc"},
+                                {"lime", "date", "zz"}
+                        },
+                        {
+                                {"peach", "berry", "dog"},
+                                {"nut", "papaya", "hi"}
+                        }
+                },
+                {
+                        {
+                                {"pineapple", "cherry", "m"},
+                                {"apricot", "q", "raspberry"}
+                        },
+                        {
+                                {"blueberry", "strawberry", "z"},
+                                {"banana", "kiwifruit", "r"}
+                        }
+                }
+        };
+        System.out.println(Arrays.toString(findShortestString(arrayOfTask20)));
+    }
+
+    // Task 16: Return the total count of strings that contain only lowercase letters.
+    // Note: you can use Character.isLowerCase() to decide whether a character
+    // is lowercase or not.
+    public static int countLowerCaseWords(String[][][][] array) {
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    for (int l = 0; l < array[i][j][k].length; l++) {
+                        String word = array[i][j][k][l];
+                        if (word != null && !word.isEmpty()) {
+                            boolean temporaryAllow = true;
+                            for (int n = 0; n < word.length(); n++) {
+                                if (!Character.isLowerCase(word.charAt(n))) {
+                                    temporaryAllow = false;
+                                    break;
+                                }
+                            }
+                            if (temporaryAllow) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+
+    // Task 17: Return a new 4D array with the same structure, where each string is reversed.
+    public static String[][][][] calculateArraySizeOfTask17(String[][][][] array) {
+        String[][][][] result = new String[array.length][][][];
+
+        for (int i = 0; i < array.length; i++) {
+            result[i] = new String[array[i].length][][];
+            for (int j = 0; j < array[i].length; j++) {
+                result[i][j] = new String[array[i][j].length][];
+                for (int k = 0; k < array[i][j].length; k++) {
+                    result[i][j][k] = new String[array[i][j][k].length];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static String[][][][] reverseString(String[][][][] array) {
+
+        String[][][][] reversed = calculateArraySizeOfTask17(array);
+        StringBuilder temporaryWord = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    for (int l = 0; l < array[i][j][k].length; l++) {
+                        temporaryWord.append(array[i][j][k][l]);
+                        reversed[i][j][k][l] = temporaryWord.reverse().toString();
+                        temporaryWord = new StringBuilder();
+                    }
+                }
+            }
+        }
+        return reversed;
+    }
+
+    // Task 18: Return the average length of all strings, rounded down to the nearest integer.
+    public static int calculateAllStringAverage(String[][][][] array) {
+        int counter = 0;
+        int sumAllString = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    for (int l = 0; l < array[i][j][k].length; l++) {
+                        sumAllString += array[i][j][k][l].length();
+                        counter++;
+                    }
+                }
+            }
+        }
+        return sumAllString / counter;
+    }
+
+    //Task 19: Return a list of all strings that contain more than two vowels.
+    //(you don’t have to consider non-English vowels, only a, e, i, o and u).
+    public static boolean isVowel(char c) {
+        char b = Character.toLowerCase(c);
+        return b == 'a' || b == 'e' || b == 'i' || b == 'o' || b == 'u';
+    }
+
+    public static int calculateArraySizeOfTask19(String[][][][] array) {
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    for (int l = 0; l < array[i][j][k].length; l++) {
+                        int counterVowel = 0;
+                        for (int m = 0; m < array[i][j][k][l].length(); m++) {
+                            if (isVowel(array[i][j][k][l].charAt(m))) {
+                                counterVowel++;
+                            }
+                        }
+                        if (counterVowel > 2) {
+                            counter++;
+                        }
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static String[] getStringsWithTwoVowel(String[][][][] array) {
+        int index = 0;
+        String[] result = new String[calculateArraySizeOfTask19(array)];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    for (int l = 0; l < array[i][j][k].length; l++) {
+                        int counterVowel = 0;
+                        for (int m = 0; m < array[i][j][k][l].length(); m++) {
+                            if (isVowel(array[i][j][k][l].charAt(m))) {
+                                counterVowel++;
+                            }
+                        }
+                        if (counterVowel > 2) {
+                            result[index++] = array[i][j][k][l];
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    // Task 20: Return the shortest string across all dimensions.
+    public static int calculateArraySizeOfTak20(String[][][][] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static String[] findShortestString(String[][][][] array) {
+        String[] result = new String[calculateArraySizeOfTak20(array)];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    String[] words = array[i][j][k];
+                    String temporaryWord = words[0];
+                    for (int l = 1; l < words.length; l++) {
+                        if (!temporaryWord.isEmpty() && words[l].length() < temporaryWord.length()) {
+                            temporaryWord = words[l];
+                        }
+                    }
+                    result[index++] = temporaryWord;
+                }
+            }
+        }
+        return result;
+    }
+
+}
--- array-fun-4d/src/MainTest.java
+++ array-fun-4d/src/MainTest.java
@@ -1,4 +1,242 @@
-package PACKAGE_NAME;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 public class MainTest {
+    // Task 16:
+    @Test
+    void shouldCountLowerCaseWords() {
+        String[][][][] arrayOfTask16 = {
+                {
+                        {
+                                {"hello", "world", "Java"},
+                                {"apple", "banana", "TREE"}
+                        },
+                        {
+                                {"test", "data", "shall"},
+                                {"code", "snippets", "123"}
+                        }
+                },
+                {
+                        {
+                                {"lower", "CASE", "MixEd"},
+                                {"yes", "no", "maybe"}
+                        },
+                        {
+                                {"only", "lowercase", "strings"},
+                                {"OKAY", "fine", "great"}
+                        }
+                }
+        };
+        assertEquals(18, Main.countLowerCaseWords(arrayOfTask16));
+    }
+
+    // Task 17:
+    @Test
+    void shouldReverseString() {
+        String[][][][] arrayOfTask17 = {
+                {
+                        {
+                                {"apple", "banana"},
+                                {"cherry", "date"}
+                        },
+                        {
+                                {"elephant", "fish"},
+                                {"grape", "hippo"}
+                        }
+                },
+                {
+                        {
+                                {"iguana", "jaguar"},
+                                {"koala", "lemur"}
+                        },
+                        {
+                                {"monkey", "narwhal"},
+                                {"octopus", "penguin"}
+                        }
+                }
+        };
+        String[][][][] expected = {
+                {
+                        {
+                                {"elppa", "ananab"},
+                                {"yrrehc", "etad"}
+                        },
+                        {
+                                {"tnahpele", "hsif"},
+                                {"eparg", "oppih"}
+                        }
+                },
+                {
+                        {
+                                {"anaugi", "raugaj"},
+                                {"alaok", "rumel"}
+                        },
+                        {
+                                {"yeknom", "lahwran"},
+                                {"supotco", "niugnep"}
+                        }
+                }
+        };
+        assertTrue(Arrays.deepEquals(expected, Main.reverseString(arrayOfTask17)));
+    }
+
+    // Task 18:
+    @Test
+    void shouldCalculateAllStringAverage() {
+        String[][][][] arrayOfTask18 = {
+                {
+                        {
+                                {"hello", "world"},
+                                {"java", "rocks"}
+                        },
+                        {
+                                {"foo", "bar", "baz"}
+                        }
+                },
+                {
+                        {
+                                {"openai"},
+                                {"gpt", "chat"}
+                        }
+                }
+        };
+        assertEquals(4, Main.calculateAllStringAverage(arrayOfTask18));
+    }
+
+    // Task 19:
+    @Test
+    void shouldCalculateArraySizeOfTask19() {
+        String[][][][] arrayOfTask19 = {
+                {
+                        {
+                                {"cat", "education"},
+                                {"sky", "audio"}
+                        },
+                        {
+                                {"tree", "why"},
+                                {"unique", "xylophone"}
+                        }
+                },
+                {
+                        {
+                                {"run", "queue"},
+                                {"box", "algorithm"}
+                        },
+                        {
+                                {"python", "java"},
+                                {"umbrella", "oop"}
+                        }
+                }
+        };
+        assertEquals(7, Main.calculateArraySizeOfTask19(arrayOfTask19));
+    }
+
+    @Test
+    void shouldGetStringsWithTwoVowel() {
+        String[][][][] arrayOfTask19 = {
+                {
+                        {
+                                {"cat", "education"},
+                                {"sky", "audio"}
+                        },
+                        {
+                                {"tree", "why"},
+                                {"unique", "xylophone"}
+                        }
+                },
+                {
+                        {
+                                {"run", "queue"},
+                                {"box", "algorithm"}
+                        },
+                        {
+                                {"python", "java"},
+                                {"umbrella", "oop"}
+                        }
+                }
+        };
+        String[] expected = {"education", "audio", "unique", "xylophone", "queue", "algorithm", "umbrella"};
+        assertArrayEquals(expected, Main.getStringsWithTwoVowel(arrayOfTask19));
+    }
+
+    // Task 20:
+    @Test
+    void shouldCalculateArraySizeOfTak20() {
+        String[][][][] arrayOfTask20Test
+                = {
+                {
+                        {
+                                {"apple", "banana", "kiwi"},
+                                {"orange", "pear", "a"}
+                        },
+                        {
+                                {"grape", "plum", "x"},
+                                {"melon", "fig", "b"}
+                        }
+                },
+                {
+                        {
+                                {"mango", "lemon", "abc"},
+                                {"lime", "date", "zz"}
+                        },
+                        {
+                                {"peach", "berry", "dog"},
+                                {"nut", "papaya", "hi"}
+                        }
+                },
+                {
+                        {
+                                {"pineapple", "cherry", "m"},
+                                {"apricot", "q", "raspberry"}
+                        },
+                        {
+                                {"blueberry", "strawberry", "z"},
+                                {"banana", "kiwifruit", "r"}
+                        }
+                }
+        };
+        assertEquals(12, Main.calculateArraySizeOfTak20(arrayOfTask20Test));
+    }
+
+    @Test
+    void shouldFindShortestString() {
+        String[][][][] arrayOfTask20 = {
+                {
+                        {
+                                {"apple", "banana", "kiwi"},
+                                {"orange", "pear", "a"}
+                        },
+                        {
+                                {"grape", "plum", "x"},
+                                {"melon", "fig", "b"}
+                        }
+                },
+                {
+                        {
+                                {"mango", "lemon", "abc"},
+                                {"lime", "date", "zz"}
+                        },
+                        {
+                                {"peach", "berry", "dog"},
+                                {"nut", "papaya", "hi"}
+                        }
+                },
+                {
+                        {
+                                {"pineapple", "cherry", "m"},
+                                {"apricot", "q", "raspberry"}
+                        },
+                        {
+                                {"blueberry", "strawberry", "z"},
+                                {"banana", "kiwifruit", "r"}
+                        }
+                }
+        };
+        String[] expected = {"kiwi", "a", "x", "b", "abc", "zz", "dog", "hi", "m", "q", "z", "r",};
+        assertArrayEquals(expected, Main.findShortestString(arrayOfTask20));
+    }
+
 }
