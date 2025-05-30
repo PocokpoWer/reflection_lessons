0a58649533c2e299e1aa8d9d1217ea35cebb6c09
Tibi <farkas.tibor1105@gmail.com>
2025-05-30 04:37:16 +0200

    add recursion method

---
+++ collatz-sequence-recursion/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        collatzSequence(6);
+    }
+
+    public static void collatzSequence(int number) {
+        System.out.print(number);
+        if (number != 1) {
+            System.out.print(", ");
+            if (number % 2 == 0) {
+                collatzSequence(number / 2);
+            } else {
+                collatzSequence(3 * number + 1);
+            }
+        }
+    }
+}
