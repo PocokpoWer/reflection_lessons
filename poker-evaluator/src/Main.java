5a3666a74a798dcfed315680f880314aa043a7a9
Tibi <farkas.tibor1105@gmail.com>
2025-08-27 15:30:09 +0200

    create poker game

--- poker-evaluator/src/Main.java
+++ poker-evaluator/src/Main.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InvalidHandException {
+        // poker cards
         List<Card> cards = Arrays.asList(
                 new Card(CardColour.PIROS, CardValue.ASZ),
                 new Card(CardColour.PIROS, CardValue.KIRALY),
